5da519174d7bbd003add7cfa7eefbd1d35a70185
Walter Davis <waldavis@mail.med.upenn.edu>
2020-10-16 17:46:33 -0400

    more tidying up

--- lib/generators/nested_fields_stimulus/install_generator.rb
+++ lib/generators/nested_fields_stimulus/install_generator.rb
@@ -8,8 +8,14 @@ module NestedFieldsStimulus
 
     desc "This generator installs the Stimulus controller for nested forms, and optionally, Stimulus itself."
 
-    def create_initializer_file
+    def create_controller
       template "nested_fields_stimulus_controller.js.erb", "app/javascript/controllers/nested_fields_stimulus_controller.js"
+    end
+    
+    private
+    
+    def rails_major
+      Rails.version.split('.').first.to_i
     end
   end
 end
--- lib/nested_fields_stimulus.rb
+++ lib/nested_fields_stimulus.rb
@@ -4,5 +4,4 @@ require "nested_fields_stimulus/engine"
 module NestedFieldsStimulus
   require 'nested_fields_stimulus/engine' if defined?(Rails)
   class Error < StandardError; end
-  # Your code goes here...
 end
